4aa82a79377b00186a0304ee60ef25cb60c58d35
Nicolas <nicolassoares.santos@outlook.com>
2024-05-04 17:32:45 -0300

    Este commit adiciona o guia passo a passo para a instalação da biblioteca necessária para a conexão MQTT, juntamente com o início do script para estabelecer essa conexão. Além disso, inclui a primeira versão funcional do script responsável pela leitura da umidade, verificada e operando conforme esperado.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
-#include <Arduino.h>
+#include <Arduino.h> // Inclui a biblioteca padrão do Arduino
 
-// put function declarations here:
-int myFunction(int, int);
+#define sensor 33 // Define o pino do sensor de umidade do solo - 33 corresponds to GPIO33
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600); // Inicializa a comunicação serial com a velocidade de 9600 bauds
+}
+
+void soilMoisture() {
+  int value = analogRead(sensor); // Lê o valor do sensor de umidade do solo
+  value = map(value, 0, 4095, 0, 100); // Mapeia o valor lido para o intervalo de 0 a 100
+  value = (value - 100) * -1; // Inverte o valor para que uma leitura maior indique um solo mais úmido
+  Serial.print("Umidade: "); // Imprime o valor lido do sensor de umidade do solo
+  Serial.print(value); // Imprime o valor de umidade do solo
+  Serial.println("%"); // Imprime o símbolo de percentagem
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  soilMoisture(); // Chama a função de leitura do sensor de umidade do solo
+  delay(1000); // Aguarda 1 segundo antes de fazer a próxima leitura
 }
