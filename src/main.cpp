bee346130aec79841935e0b21328e5fd3ae3caed
Nicolas <nicolassoares.santos@outlook.com>
2024-05-09 15:38:55 -0300

    Implementação de funcionalidade para controle de bomba de água

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,40 @@
 #include <PubSubClient.h>
 
 // Definição das credenciais da rede Wi-Fi
-const char* ssid = "NOME DO SEU WIFI"; // Substitua pelo nome da sua rede Wi-Fi
-const char* password = "SENHA DO SEU WIFI"; // Substitua pela senha da sua rede Wi-Fi
+const char* ssid = "NGNL"; // Nome da rede Wi-Fi
+const char* password = "Liso9445"; // Senha da rede Wi-Fi
 
 // Configurações do servidor MQTT (broker)
 const char* mqtt_server = "broker.emqx.io"; // Endereço do servidor MQTT
 const int mqtt_port = 1883;  // Porta padrão MQTT
 
-WiFiClient espClient; // Cria uma instância do cliente WiFi
-PubSubClient client(espClient); // Cria uma instância do PubSubClient para comunicação MQTT
+WiFiClient espClient; // Cliente WiFi
+PubSubClient mqttClient(espClient); // Cliente MQTT
 
-#define sensor 33
-#define humidityTopic "umidade/solo" // Tópico utilizado para publicar a umidade do solo
-#define controlTopic "controle/dispositivo" // Tópico utilizado para controle do dispositivo
+// Definições dos pinos e tópicos MQTT
+#define SOIL_SENSOR_PIN 33
+#define HUMIDITY_TOPIC "umidade/solo" // Tópico da umidade do solo
+#define CONTROL_TOPIC "controle/dispositivo" // Tópico de controle do dispositivo
 
-bool deviceRunning = true; // Variável para controlar o estado do dispositivo
+#define RELAY_PIN 32
+#define MIN_HUMIDITY 45
+#define MAX_HUMIDITY 60
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  // Converte o payload para uma string
+bool deviceRunning = true; // Estado do dispositivo
+
+// Função de callback para mensagens MQTT recebidas
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
   String message;
   for (int i = 0; i < length; i++) {
     message += (char)payload[i];
   }
-  
-  // Verifica se a mensagem recebida é para controlar o dispositivo
-  if (String(topic) == controlTopic) {
+  // Verifica se a mensagem é para controlar o dispositivo
+  if (String(topic) == CONTROL_TOPIC) {
     if (message == "ligar") {
-      deviceRunning = true; // Ligar o dispositivo
+      deviceRunning = true; // Liga o dispositivo
       Serial.println("Dispositivo ligado");
     } else if (message == "desligar") {
-      deviceRunning = false; // Desligar o dispositivo
+      deviceRunning = false; // Desliga o dispositivo
       Serial.println("Dispositivo desligado");
     }
   }
@@ -40,9 +44,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 void setup() {
   Serial.begin(9600); // Inicializa a comunicação serial
+  WiFi.begin(ssid, password); // Conecta-se à rede Wi-Fi
   
-  // Conexão à rede Wi-Fi
-  WiFi.begin(ssid, password);
+  // Aguarda até que a conexão seja estabelecida
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -51,61 +55,65 @@ void setup() {
   Serial.println("WiFi conectado");
   Serial.println("Endereço IP: ");
   Serial.println(WiFi.localIP()); // Exibe o endereço IP atribuído ao dispositivo
+
+  mqttClient.setServer(mqtt_server, mqtt_port); // Configura o servidor MQTT
+  mqttClient.setCallback(mqttCallback); // Define a função de callback para mensagens MQTT recebidas
   
-  // Configura o servidor MQTT
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback); // Define a função de callback para receber mensagens MQTT
+  pinMode(RELAY_PIN, OUTPUT); // Configura o pino do relé como saída
+  digitalWrite(RELAY_PIN, LOW); // Desliga o relé inicialmente
 }
 
+// Função para enviar a umidade do solo via MQTT
 void sendHumidity() {
-  // Verifica se o dispositivo está ligado antes de enviar a leitura da umidade
-  if (deviceRunning) {
-    // Leitura do sensor de umidade
-    int value = analogRead(sensor);
-    // Mapeamento do valor lido para uma faixa de 0 a 100
-    value = map(value, 0, 4095, 0, 100);
-    // Inversão do valor para uma escala de 0 a 100
-    value = (value - 100) * -1;
+  if (deviceRunning) { // Verifica se o dispositivo está ligado
+    int humidityValue = analogRead(SOIL_SENSOR_PIN); // Realiza a leitura do sensor de umidade
+    humidityValue = map(humidityValue, 0, 4095, 0, 100); // Mapeia o valor lido para uma faixa de 0 a 100
+    humidityValue = (humidityValue - 100) * -1; // Inverte o valor para uma escala de 0 a 100
     Serial.print("Umidade: ");
-    Serial.print(value);
+    Serial.print(humidityValue);
     Serial.println("%");
 
-    // Verifica se a conexão com o broker MQTT está estabelecida
-    if (client.connected()) {
-      // Publica a umidade no tópico especificado apenas se a conexão estiver ativa
-      client.publish(humidityTopic, String(value).c_str());
+    // Controla o relé com base na umidade medida
+    if (humidityValue < MIN_HUMIDITY) {
+      digitalWrite(RELAY_PIN, HIGH); // Liga o relé se a umidade estiver abaixo do limite mínimo
+    } else if (humidityValue >= MAX_HUMIDITY) {
+      digitalWrite(RELAY_PIN, LOW); // Desliga o relé se a umidade atingir o limite máximo
+    }
+
+    // Verifica se a conexão MQTT está ativa e publica a umidade do solo
+    if (mqttClient.connected()) {
+      mqttClient.publish(HUMIDITY_TOPIC, String(humidityValue).c_str());
       Serial.println("Umidade enviada com sucesso");
     } else {
       Serial.println("Erro: Não foi possível enviar a umidade, conexão MQTT não estabelecida");
     }
   } else {
+    digitalWrite(RELAY_PIN, LOW); // Desliga o relé se o dispositivo estiver desligado
     Serial.println("Dispositivo desligado, não é possível enviar a umidade");
   }
 }
 
-void reconnect() {
-  // Loop até que estejamos reconectados
-  while (!client.connected()) {
+// Função para reconectar ao servidor MQTT
+void mqttReconnect() {
+  while (!mqttClient.connected()) {
     Serial.print("Tentando conexão MQTT...");
-    // Tenta conectar
-    if (client.connect("ESP32Cliente")) {
+    if (mqttClient.connect("ESP32Cliente")) { // Tenta conectar ao broker MQTT
       Serial.println("Conectado ao broker MQTT");
-      // Inscreva-se no tópico de controle
-      client.subscribe(controlTopic);
+      mqttClient.subscribe(CONTROL_TOPIC); // Inscreve-se no tópico de controle
     } else {
       Serial.print("Falha na conexão, código de retorno=");
-      Serial.print(client.state());
+      Serial.print(mqttClient.state());
       Serial.println(", tentando novamente em 5 segundos");
-      delay(5000); // Espera 5 segundos antes de tentar novamente
+      delay(5000); // Espera 5 segundos antes de tentar reconectar
     }
   }
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
+  if (!mqttClient.connected()) {
+    mqttReconnect(); // Reconecta ao servidor MQTT se a conexão for perdida
   }
-  client.loop(); // Mantém a comunicação com o broker MQTT ativa
-  sendHumidity(); // Envia a leitura da umidade do solo
+  mqttClient.loop(); // Mantém a comunicação com o servidor MQTT
+  sendHumidity(); // Envia a umidade do solo
   delay(1000); // Aguarda 1 segundo antes de realizar uma nova leitura
 }
